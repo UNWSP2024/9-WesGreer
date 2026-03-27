0339fa0cbd655cf26037c7aac293312d3ef08083
WesGreer <wsgreer878@gmail.com>
2026-03-27 14:42:39 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,5 +1,16 @@
-# Program #2: Random Number File Writer
-# Write a program that writes a series of random numbers (up to 1000) to a file.
-# Each random number should be in the range of 1 through 500. 
-# The application should let the user specify how many random numbers the file will hold 
-# (up to 1000).
+# Random Number File Program
+# Written By Wesley Greer on 3/27/2026
+
+import random
+
+print('This program allows you to write up to 1000 random numbers to a file.')
+with open('numbers.txt' 'w') as num:
+    try:
+        file_size = int(input('How many random numbers would you like to write to the file? '))
+        if file_size > 1000 or file_size < 0:
+            print('Please enter a number equal to or less than 1000, and make sure it is a positive integer.')
+            file_size = int(input('How many random numbers would you like to write to the file? '))
+        for n in range(file_size):
+            num.write(random.randint(1,500),'/n')
+    except ValueError:
+        print('Error: You must enter an integer. Your number cannot have decimals. (e.g. 3.5, .25)')
